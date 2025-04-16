84c9431e0be49f14efaf55e8739c4931ce0f1592
KKS <ksiki1@naver.com>
2025-04-16 15:11:24 +0900

    모든 카테고리 리사이클러뷰

--- app/src/main/java/com/example/gptrecipeapp/RecIngredientsFragment.kt
+++ app/src/main/java/com/example/gptrecipeapp/RecIngredientsFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.example.gptrecipeapp.adapter.RecIngredientsAdapter
 import com.example.gptrecipeapp.databinding.FragmentRecIngredientsBinding
 import com.example.gptrecipeapp.model.IngredientsModel
@@ -14,12 +15,12 @@ class RecIngredientsFragment : Fragment() {
     private var _binding: FragmentRecIngredientsBinding? = null
     private val binding get() = _binding!!
 
-    private lateinit var meatAdapter: RecIngredientsAdapter
-    private lateinit var seafoodAdapter: RecIngredientsAdapter
-    private lateinit var vegetableAdapter: RecIngredientsAdapter
-    private lateinit var fruitAdapter: RecIngredientsAdapter
-    private lateinit var processedAdapter: RecIngredientsAdapter
-    private lateinit var etcAdapter: RecIngredientsAdapter
+    private val meatAdapter = RecIngredientsAdapter()
+    private val seafoodAdapter = RecIngredientsAdapter()
+    private val vegetableAdapter = RecIngredientsAdapter()
+    private val fruitAdapter = RecIngredientsAdapter()
+    private val processedAdapter = RecIngredientsAdapter()
+    private val etcAdapter = RecIngredientsAdapter()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -33,13 +34,32 @@ class RecIngredientsFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.btnBack.setOnClickListener {
-            requireActivity().onBackPressed()
+            findNavController().popBackStack()
         }
 
         with(binding.rvMeatList) {
-            meatAdapter = RecIngredientsAdapter()
             adapter = meatAdapter
             meatAdapter.submitList(getMeatList())
+        }
+        with(binding.rvSeafoodList) {
+            adapter = seafoodAdapter
+            seafoodAdapter.submitList(getSeafoodList())
+        }
+        with(binding.rvVegetableList) {
+            adapter = vegetableAdapter
+            vegetableAdapter.submitList(getVegetableList())
+        }
+        with(binding.rvFruitList) {
+            adapter = fruitAdapter
+            fruitAdapter.submitList(getFruitList())
+        }
+        with(binding.rvProcessedList) {
+            adapter = processedAdapter
+            processedAdapter.submitList(getProcessedList())
+        }
+        with(binding.rvEtcList) {
+            adapter = etcAdapter
+            etcAdapter.submitList(getEtcList())
         }
     }
 
@@ -55,6 +75,106 @@ class RecIngredientsFragment : Fragment() {
             IngredientsModel(ingredients = "소고기", initialIsSelected = false),
             IngredientsModel(ingredients = "양고기", initialIsSelected = false),
             IngredientsModel(ingredients = "오리고기", initialIsSelected = false)
+        )
+    }
+
+    private fun getSeafoodList(): MutableList<IngredientsModel> {
+        return mutableListOf(
+            IngredientsModel(ingredients = "전복", initialIsSelected = false),
+            IngredientsModel(ingredients = "굴", initialIsSelected = false),
+            IngredientsModel(ingredients = "꽃게", initialIsSelected = false),
+            IngredientsModel(ingredients = "낙지", initialIsSelected = false),
+            IngredientsModel(ingredients = "쭈꾸미", initialIsSelected = false),
+            IngredientsModel(ingredients = "문어", initialIsSelected = false),
+            IngredientsModel(ingredients = "오징어", initialIsSelected = false),
+            IngredientsModel(ingredients = "미역", initialIsSelected = false),
+            IngredientsModel(ingredients = "새우", initialIsSelected = false),
+            IngredientsModel(ingredients = "전어", initialIsSelected = false),
+            IngredientsModel(ingredients = "연어", initialIsSelected = false),
+            IngredientsModel(ingredients = "갈치", initialIsSelected = false),
+            IngredientsModel(ingredients = "꽁치", initialIsSelected = false),
+            IngredientsModel(ingredients = "고등어", initialIsSelected = false),
+            IngredientsModel(ingredients = "멸치", initialIsSelected = false),
+            IngredientsModel(ingredients = "조개", initialIsSelected = false),
+            IngredientsModel(ingredients = "바지락", initialIsSelected = false),
+            IngredientsModel(ingredients = "꼬막", initialIsSelected = false),
+            IngredientsModel(ingredients = "골뱅이", initialIsSelected = false),
+        )
+    }
+
+    private fun getVegetableList(): MutableList<IngredientsModel> {
+        return mutableListOf(
+            IngredientsModel(ingredients = "김치", initialIsSelected = false),
+            IngredientsModel(ingredients = "배추", initialIsSelected = false),
+            IngredientsModel(ingredients = "양배추", initialIsSelected = false),
+            IngredientsModel(ingredients = "상추", initialIsSelected = false),
+            IngredientsModel(ingredients = "깻잎", initialIsSelected = false),
+            IngredientsModel(ingredients = "시금치", initialIsSelected = false),
+            IngredientsModel(ingredients = "대파", initialIsSelected = false),
+            IngredientsModel(ingredients = "가지", initialIsSelected = false),
+            IngredientsModel(ingredients = "무", initialIsSelected = false),
+            IngredientsModel(ingredients = "대파", initialIsSelected = false),
+            IngredientsModel(ingredients = "마늘", initialIsSelected = false),
+            IngredientsModel(ingredients = "양파", initialIsSelected = false),
+            IngredientsModel(ingredients = "버섯", initialIsSelected = false),
+            IngredientsModel(ingredients = "마늘", initialIsSelected = false),
+            IngredientsModel(ingredients = "콩나물", initialIsSelected = false),
+            IngredientsModel(ingredients = "토마토", initialIsSelected = false),
+            IngredientsModel(ingredients = "파프리카", initialIsSelected = false),
+            IngredientsModel(ingredients = "두부", initialIsSelected = false),
+            IngredientsModel(ingredients = "콩", initialIsSelected = false),
+            IngredientsModel(ingredients = "감자", initialIsSelected = false),
+            IngredientsModel(ingredients = "고구마", initialIsSelected = false),
+            IngredientsModel(ingredients = "옥수수", initialIsSelected = false),
+        )
+    }
+
+    private fun getFruitList(): MutableList<IngredientsModel> {
+        return mutableListOf(
+            IngredientsModel(ingredients = "사과", initialIsSelected = false),
+            IngredientsModel(ingredients = "바나나", initialIsSelected = false),
+            IngredientsModel(ingredients = "배", initialIsSelected = false),
+            IngredientsModel(ingredients = "복숭아", initialIsSelected = false),
+            IngredientsModel(ingredients = "딸기", initialIsSelected = false),
+            IngredientsModel(ingredients = "포도", initialIsSelected = false),
+            IngredientsModel(ingredients = "망고", initialIsSelected = false),
+            IngredientsModel(ingredients = "블루베리", initialIsSelected = false),
+            IngredientsModel(ingredients = "파인애플", initialIsSelected = false),
+            IngredientsModel(ingredients = "레몬", initialIsSelected = false),
+            IngredientsModel(ingredients = "라임", initialIsSelected = false),
+            IngredientsModel(ingredients = "아보카도"),
+        )
+    }
+
+    private fun getProcessedList(): MutableList<IngredientsModel> {
+        return mutableListOf(
+            IngredientsModel(ingredients = "계란", initialIsSelected = false),
+            IngredientsModel(ingredients = "메추리알", initialIsSelected = false),
+            IngredientsModel(ingredients = "치즈", initialIsSelected = false),
+            IngredientsModel(ingredients = "요거트", initialIsSelected = false),
+            IngredientsModel(ingredients = "스팸", initialIsSelected = false),
+            IngredientsModel(ingredients = "햄", initialIsSelected = false),
+            IngredientsModel(ingredients = "베이컨", initialIsSelected = false),
+            IngredientsModel(ingredients = "소시지", initialIsSelected = false),
+            IngredientsModel(ingredients = "만두", initialIsSelected = false),
+            IngredientsModel(ingredients = "순대", initialIsSelected = false),
+            IngredientsModel(ingredients = "참치캔", initialIsSelected = false),
+        )
+    }
+
+    private fun getEtcList(): MutableList<IngredientsModel> {
+        return mutableListOf(
+            IngredientsModel(ingredients = "라면", initialIsSelected = false),
+            IngredientsModel(ingredients = "스파게티면", initialIsSelected = false),
+            IngredientsModel(ingredients = "소면", initialIsSelected = false),
+            IngredientsModel(ingredients = "당면", initialIsSelected = false),
+            IngredientsModel(ingredients = "우동면", initialIsSelected = false),
+            IngredientsModel(ingredients = "수제비", initialIsSelected = false),
+            IngredientsModel(ingredients = "가래떡", initialIsSelected = false),
+            IngredientsModel(ingredients = "떡국떡", initialIsSelected = false),
+            IngredientsModel(ingredients = "바게트", initialIsSelected = false),
+            IngredientsModel(ingredients = "베이글", initialIsSelected = false),
+            IngredientsModel(ingredients = "식빵", initialIsSelected = false),
         )
     }
 }
