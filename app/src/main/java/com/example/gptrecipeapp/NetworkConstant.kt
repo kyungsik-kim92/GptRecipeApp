71edd25ef40bf0dbf10435350b9d1f64282ef905
KKS <ksiki1@naver.com>
2025-04-18 23:40:15 +0900

    NetworkConstant

---
+++ app/src/main/java/com/example/gptrecipeapp/NetworkConstant.kt
@@ -0,0 +1,7 @@
+package com.example.gptrecipeapp
+
+object NetworkConstant {
+
+    const val SERVER_BASE_URL = "https://api.openai.com/"
+    const val GPT_TOKEN = "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"
+}
