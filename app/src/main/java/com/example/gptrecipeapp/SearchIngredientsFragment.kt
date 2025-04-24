c91185f573c2f27c1af7fd2bfb6aa53c87aab11c
KKS <ksiki1@naver.com>
2025-04-24 23:04:59 +0900

    SearchIngredientsFragment

--- app/src/main/java/com/example/gptrecipeapp/SearchIngredientsFragment.kt
+++ app/src/main/java/com/example/gptrecipeapp/SearchIngredientsFragment.kt
@@ -1,6 +1,27 @@
 package com.example.gptrecipeapp
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.gptrecipeapp.databinding.FragmentSearchIngredientsBinding
 
 class SearchIngredientsFragment : Fragment() {
+    private var _binding: FragmentSearchIngredientsBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentSearchIngredientsBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
 }
