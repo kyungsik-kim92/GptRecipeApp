ea10b9d95a5a9bd66a392da3480c9a46fb047418
KKS <ksiki1@naver.com>
2025-05-10 22:52:22 +0900

    SearchKeywordAdapter

---
+++ app/src/main/java/com/example/gptrecipeapp/OnItemSelectedListener.kt
@@ -0,0 +1,5 @@
+package com.example.gptrecipeapp
+
+interface OnItemSelectedListener {
+    fun onItemSelected(searchKeyword: String)
+}
---
+++ app/src/main/java/com/example/gptrecipeapp/ui/adapter/SearchKeywordAdapter.kt
@@ -0,0 +1,50 @@
+package com.example.gptrecipeapp.ui.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.example.gptrecipeapp.OnItemSelectedListener
+import com.example.gptrecipeapp.databinding.ItemSearchKeywordBinding
+
+class SearchKeywordAdapter : ListAdapter<String, SearchKeywordViewHolder>(diffUtil) {
+
+    private lateinit var listener: OnItemSelectedListener
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SearchKeywordViewHolder {
+        val binding =
+            ItemSearchKeywordBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return SearchKeywordViewHolder(binding, listener)
+    }
+
+    override fun onBindViewHolder(holder: SearchKeywordViewHolder, position: Int) {
+        holder.bind(getItem(position))
+    }
+
+    fun setOnItemSelectedListener(listener: OnItemSelectedListener) {
+        this.listener = listener
+    }
+
+    companion object {
+        val diffUtil = object : DiffUtil.ItemCallback<String>() {
+            override fun areItemsTheSame(oldItem: String, newItem: String) =
+                oldItem == newItem
+
+            override fun areContentsTheSame(oldItem: String, newItem: String) =
+                oldItem == newItem
+        }
+    }
+}
+
+class SearchKeywordViewHolder(
+    private val binding: ItemSearchKeywordBinding,
+    private val listener: OnItemSelectedListener
+) : RecyclerView.ViewHolder(binding.root) {
+    fun bind(keyword: String) {
+        binding.searchKeyword = keyword
+        itemView.setOnClickListener {
+            listener.onItemSelected(keyword)
+        }
+    }
+}
