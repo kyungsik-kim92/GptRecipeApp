ce84b4c57954baf57bec4178e377817448cbd8c0
KKS <ksiki1@naver.com>
2025-05-15 23:20:57 +0900

    RecipeAdapter

---
+++ app/src/main/java/com/example/gptrecipeapp/ui/adapter/RecipeAdapter.kt
@@ -0,0 +1,39 @@
+package com.example.gptrecipeapp.ui.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.example.gptrecipeapp.databinding.ItemRecipeBinding
+import com.example.gptrecipeapp.model.RecipeModel
+
+class RecipeAdapter : ListAdapter<RecipeModel, RecipeViewHolder>(diffUtil) {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecipeViewHolder {
+        val binding = ItemRecipeBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return RecipeViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: RecipeViewHolder, position: Int) {
+        holder.bind(getItem(position))
+    }
+
+    companion object {
+        private val diffUtil = object : DiffUtil.ItemCallback<RecipeModel>() {
+            override fun areItemsTheSame(oldItem: RecipeModel, newItem: RecipeModel) =
+                oldItem == newItem
+
+            override fun areContentsTheSame(oldItem: RecipeModel, newItem: RecipeModel) =
+                oldItem == newItem
+
+        }
+    }
+}
+
+class RecipeViewHolder(
+    val binding: ItemRecipeBinding
+) : RecyclerView.ViewHolder(binding.root) {
+    fun bind(item: RecipeModel) {
+        binding.recipeModel = item
+    }
+}
