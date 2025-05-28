80f14b225b0218e7ed046316cdaa022d45e59947
KKS <ksiki1@naver.com>
2025-05-29 01:48:30 +0900

    DataSource

--- app/src/main/java/com/example/gptrecipeapp/DataSource.kt
+++ app/src/main/java/com/example/gptrecipeapp/DataSource.kt
@@ -1,9 +1,19 @@
 package com.example.gptrecipeapp
 
 import com.example.gptrecipeapp.model.GPT
+import com.example.gptrecipeapp.room.entity.LocalRecipeEntity
 
-//interface DataSource {
-//    suspend fun getGptResponse(
-//        body: GptRequestParam
-//    ): GPT
-//}
+interface DataSource {
+    suspend fun getGptResponse(
+        body: GptRequestParam
+    ): GPT
+
+    suspend fun getAll(): List<LocalRecipeEntity>
+
+    suspend fun insertRecipe(recipe: LocalRecipeEntity): Long
+
+    suspend fun deleteRecipe(id: Long)
+
+    suspend fun findRecipe(id: Long): LocalRecipeEntity?
+
+}
--- app/src/main/java/com/example/gptrecipeapp/DataSourceImpl.kt
+++ app/src/main/java/com/example/gptrecipeapp/DataSourceImpl.kt
@@ -1,9 +1,30 @@
 package com.example.gptrecipeapp
 
 import com.example.gptrecipeapp.model.GPT
+import com.example.gptrecipeapp.room.dao.RecipeDao
+import com.example.gptrecipeapp.room.entity.LocalRecipeEntity
 
-//class DataSourceImpl(private val apiService: ApiService) : DataSource {
-//    override suspend fun getGptResponse(body: GptRequestParam): GPT {
-//        return apiService.getGptResponse(body)
-//    }
-//}
+class DataSourceImpl(
+    private val apiService: ApiService,
+    private val recipeDao: RecipeDao
+) : DataSource {
+    override suspend fun getGptResponse(body: GptRequestParam): GPT {
+        return apiService.getGptResponse(body)
+    }
+
+    override suspend fun getAll(): List<LocalRecipeEntity> {
+        return recipeDao.getAll()
+    }
+
+    override suspend fun insertRecipe(recipe: LocalRecipeEntity): Long {
+        return recipeDao.insertRecipe(recipe)
+    }
+
+    override suspend fun deleteRecipe(id: Long) {
+        return recipeDao.deleteRecipe(id)
+    }
+
+    override suspend fun findRecipe(id: Long): LocalRecipeEntity? {
+        return recipeDao.findRecipe(id)
+    }
+}
