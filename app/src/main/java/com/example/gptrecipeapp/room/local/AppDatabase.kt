51d99fca73515c079d66b9f98530e2d1861bc846
KKS <ksiki1@naver.com>
2025-05-29 01:48:16 +0900

    AppDatabase

---
+++ app/src/main/java/com/example/gptrecipeapp/room/local/AppDatabase.kt
@@ -0,0 +1,15 @@
+package com.example.gptrecipeapp.room.local
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import com.example.gptrecipeapp.room.CustomTypeConverters
+import com.example.gptrecipeapp.room.dao.RecipeDao
+import com.example.gptrecipeapp.room.entity.LocalRecipeEntity
+
+@Database(entities = [LocalRecipeEntity::class], version = 1, exportSchema = false)
+@TypeConverters(CustomTypeConverters::class)
+abstract class AppDatabase : RoomDatabase() {
+    abstract val recipeDao: RecipeDao
+
+}
