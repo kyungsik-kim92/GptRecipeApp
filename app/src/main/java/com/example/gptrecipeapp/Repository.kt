ff08e9973625949b51d0997e993d4524ed9987f6
KKS <ksiki1@naver.com>
2025-05-30 22:31:40 +0900

    Repository

--- app/src/main/java/com/example/gptrecipeapp/Repository.kt
+++ app/src/main/java/com/example/gptrecipeapp/Repository.kt
@@ -1,9 +1,18 @@
 package com.example.gptrecipeapp
 
 import com.example.gptrecipeapp.model.GPT
+import com.example.gptrecipeapp.room.entity.LocalRecipeEntity
 
 interface Repository {
     suspend fun getGptResponse(
         body: GptRequestParam
     ): GPT
+
+    suspend fun getAll(): List<LocalRecipeEntity>
+
+    suspend fun insertRecipe(recipe: LocalRecipeEntity): Long
+
+    suspend fun deleteRecipe(id: Long)
+
+    suspend fun findRecipe(id: Long): LocalRecipeEntity?
 }
--- app/src/main/java/com/example/gptrecipeapp/RepositoryImpl.kt
+++ app/src/main/java/com/example/gptrecipeapp/RepositoryImpl.kt
@@ -1,9 +1,26 @@
 package com.example.gptrecipeapp
 
 import com.example.gptrecipeapp.model.GPT
+import com.example.gptrecipeapp.room.entity.LocalRecipeEntity
 
-class RepositoryImpl(private val apiService: ApiService) : Repository {
+class RepositoryImpl(private val dataSource: DataSource) : Repository {
     override suspend fun getGptResponse(body: GptRequestParam): GPT {
-        return apiService.getGptResponse(body)
+        return dataSource.getGptResponse(body)
+    }
+
+    override suspend fun getAll(): List<LocalRecipeEntity> {
+        return dataSource.getAll()
+    }
+
+    override suspend fun insertRecipe(recipe: LocalRecipeEntity): Long {
+        return dataSource.insertRecipe(recipe)
+    }
+
+    override suspend fun deleteRecipe(id: Long) {
+        return dataSource.deleteRecipe(id)
+    }
+
+    override suspend fun findRecipe(id: Long): LocalRecipeEntity? {
+        return dataSource.findRecipe(id)
     }
 }
