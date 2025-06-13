3260fad93e4144e6d7d742bdc40becb610bcdd23
KKS <ksiki1@naver.com>
2025-06-14 01:27:43 +0900

    TypeConverter 수정

--- data/src/main/java/com/example/data/database/converters/TypeConverter.kt
+++ data/src/main/java/com/example/data/database/converters/TypeConverter.kt
@@ -9,35 +9,32 @@ import com.google.gson.Gson
 class CustomTypeConverters {
 
     @TypeConverter
-    fun ingredientsListToJson(value: ArrayList<IngredientsEntity>?): String? {
+    fun ingredientsListToJson(value: List<IngredientsEntity>?): String? {
         return Gson().toJson(value)
     }
 
     @TypeConverter
-    fun jsonToIngredientsList(value: String): ArrayList<IngredientsEntity>? {
+    fun jsonToIngredientsList(value: String): List<IngredientsEntity>? {
         return Gson().fromJson(value, Array<IngredientsEntity>::class.java)?.toList()
-            ?.let { ArrayList(it) }
     }
 
     @TypeConverter
-    fun recipeListToJson(value: ArrayList<RecipeEntity>?): String? {
+    fun recipeListToJson(value: List<RecipeEntity>?): String? {
         return Gson().toJson(value)
     }
 
     @TypeConverter
-    fun jsonToRecipeList(value: String): ArrayList<RecipeEntity>? {
+    fun jsonToRecipeList(value: String): List<RecipeEntity>? {
         return Gson().fromJson(value, Array<RecipeEntity>::class.java)?.toList()
-            ?.let { ArrayList(it) }
     }
 
     @TypeConverter
-    fun wellBeingRecipeListToJson(value: ArrayList<WellbeingRecipeEntity>?): String? {
+    fun wellBeingRecipeListToJson(value: List<WellbeingRecipeEntity>?): String? {
         return Gson().toJson(value)
     }
 
     @TypeConverter
-    fun jsonToWellBeingRecipeList(value: String): ArrayList<WellbeingRecipeEntity>? {
+    fun jsonToWellBeingRecipeList(value: String): List<WellbeingRecipeEntity>? {
         return Gson().fromJson(value, Array<WellbeingRecipeEntity>::class.java)?.toList()
-            ?.let { ArrayList(it) }
     }
 }
