c5ca0dfc1e36c91440eb0651433e5e52a4b10349
KKS <ksiki1@naver.com>
2025-07-25 14:41:28 +0900

    RecipeUiState SealedClass로 수정

--- presentation/src/main/java/com/example/presentation/ui/recipe/RecipeUiState.kt
+++ presentation/src/main/java/com/example/presentation/ui/recipe/RecipeUiState.kt
@@ -8,12 +8,57 @@ import kotlinx.parcelize.Parcelize
 
 
 @Parcelize
-data class RecipeUiState(
-    var id: Long,
-    var searchKeyword: String,
-    var ingredientsList: List<IngredientsModel>,
-    var recipeList: List<RecipeModel>,
-    var isLoading: Boolean,
-    var isSubscribe: Boolean,
-    var wellbeingRecipeModel: List<WellbeingRecipeModel> = emptyList(),
-) : Parcelable
+sealed class RecipeUiState : Parcelable {
+    abstract val id: Long
+    abstract val searchKeyword: String
+    abstract val ingredientsList: List<IngredientsModel>
+    abstract val recipeList: List<RecipeModel>
+    abstract val isSubscribe: Boolean
+    abstract val wellbeingRecipeModel: List<WellbeingRecipeModel>
+
+    @Parcelize
+    data class Idle(
+        override val id: Long = 0L,
+        override val searchKeyword: String = "",
+        override val ingredientsList: List<IngredientsModel> = emptyList(),
+        override val recipeList: List<RecipeModel> = emptyList(),
+        override val isSubscribe: Boolean = false,
+        override val wellbeingRecipeModel: List<WellbeingRecipeModel> = emptyList()
+    ) : RecipeUiState()
+
+    @Parcelize
+    data class Loading(
+        override val id: Long,
+        override val searchKeyword: String,
+        override val ingredientsList: List<IngredientsModel>,
+        override val recipeList: List<RecipeModel>,
+        override val isSubscribe: Boolean,
+        override val wellbeingRecipeModel: List<WellbeingRecipeModel>
+    ) : RecipeUiState()
+
+    @Parcelize
+    data class Success(
+        override val id: Long,
+        override val searchKeyword: String,
+        override val ingredientsList: List<IngredientsModel>,
+        override val recipeList: List<RecipeModel>,
+        override val isSubscribe: Boolean,
+        override val wellbeingRecipeModel: List<WellbeingRecipeModel>
+    ) : RecipeUiState()
+
+    @Parcelize
+    data class Error(
+        override val id: Long,
+        override val searchKeyword: String,
+        override val ingredientsList: List<IngredientsModel>,
+        override val recipeList: List<RecipeModel>,
+        override val isSubscribe: Boolean,
+        override val wellbeingRecipeModel: List<WellbeingRecipeModel>,
+        val message: String
+    ) : RecipeUiState()
+}
+sealed class RecipeUiEvent {
+    data class ShowSuccess(val message: String) : RecipeUiEvent()
+    data class ShowError(val message: String) : RecipeUiEvent()
+    data class RouteToWellbeing(val recipeUiState: RecipeUiState) : RecipeUiEvent()
+}
