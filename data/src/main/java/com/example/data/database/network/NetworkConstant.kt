0c87d1defd85d7b7523eb59aa2da5c15d58f67e7
KKS <ksiki1@naver.com>
2025-11-04 21:46:52 +0900

    api key 갱신

--- data/src/main/java/com/example/data/database/network/NetworkConstant.kt
+++ data/src/main/java/com/example/data/database/network/NetworkConstant.kt
@@ -3,5 +3,5 @@ package com.example.data.database.network
 object NetworkConstant {
     const val BASE_URL = "https://api.openai.com/"
     const val GPT_TOKEN =
-        "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"
+        "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"
 }
