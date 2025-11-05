36b524692a15de154d2d67fa8a66063fa5e417dc
KKS <ksiki1@naver.com>
2025-11-05 20:26:06 +0900

    AppDatabase 수정

--- data/src/main/java/com/example/data/database/AppDatabase.kt
+++ data/src/main/java/com/example/data/database/AppDatabase.kt
@@ -5,12 +5,22 @@ import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 import com.example.data.database.converters.CustomTypeConverters
 import com.example.data.database.dao.RecipeDao
+import com.example.data.database.dao.ShoppingItemDao
 import com.example.data.local.entity.LocalRecipeEntity
+import com.example.data.local.entity.ShoppingItemEntity
 
-@Database(entities = [LocalRecipeEntity::class], version = 1, exportSchema = false)
+@Database(
+    entities = [
+        LocalRecipeEntity::class,
+        ShoppingItemEntity::class
+    ],
+    version = 2,
+    exportSchema = false
+)
 @TypeConverters(CustomTypeConverters::class)
 abstract class AppDatabase : RoomDatabase() {
     abstract val recipeDao: RecipeDao
+    abstract val shoppingItemDao: ShoppingItemDao
 
     companion object {
         const val DB_NAME = "recipe.db"
