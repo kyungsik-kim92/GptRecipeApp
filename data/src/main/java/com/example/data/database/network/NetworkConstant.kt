c068d242dff5d6e9c0cb6f3505c497cd0e15bcf4
KKS <ksiki1@naver.com>
2025-11-26 18:34:05 +0900

    NetworkConstant 제거

--- data/src/main/java/com/example/data/database/network/NetworkConstant.kt
+++
@@ -1,7 +0,0 @@
-package com.example.data.database.network
-
-object NetworkConstant {
-    const val BASE_URL = "https://api.openai.com/"
-    const val GPT_TOKEN =
-        "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"
-}
